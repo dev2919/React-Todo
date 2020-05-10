af1d64a9743b00e85aa962dbf14191e6e5ba67b0
dev2919 <dev2919@gmail.com>
2020-05-10 17:44:34 +0530

    Second Commit added UI and backend fixes

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import List from './List'
+let key=0;
+
+
 
 class App extends React.Component{
 
   constructor(props) {
     super(props);
     this.state = {
-      content:[],
+      content:[{
+        des:'',
+        key:''
+      }],
       text:'',
-      key:0
+      date:'',
+      month:'',
     }    
   }
 
@@ -24,18 +31,21 @@ class App extends React.Component{
     const {content,key} = this.state;
     this.setState({
       text:'',
-      content:content.concat(this.state.text)
+      content:content.concat({
+        des:this.state.text,
+        key:Date.now()
+      })
     })
-    console.log(content);
-    
+    this.hide();
   }
 
-  delContent = () =>{
-    const {content,key} = this.state;
+  delContent = (i) =>{
+    const {content} = this.state;
     const e = document.getElementById("todo-input");
-    let item = e.value;
-    let index = content.indexOf(item);
-    console.log(content,index,item);
+    let item = content[key];
+    console.log(i);
+    let index = content.findIndex(x => x.key == i);
+    
     if (index !== -1) content.splice(index, 1);
     this.setState({
       content:content
@@ -43,16 +53,105 @@ class App extends React.Component{
   }
 
 
+  expand = ()=>{
+    document.getElementById("over").classList.add("mystyle");    
+    document.getElementById("over").classList.remove("hidden");
+    document.getElementById("time-smile").style.display="none";
+    document.getElementById("hide-content").style.display="block";
+
+    setTimeout(() => {
+      document.getElementById("hide-content").style.opacity="1";
+    }, 500);
+
+
+    }
+
+  hide = ()=>{
+    document.getElementById("over").classList.add("hidden");    
+    document.getElementById("over").classList.remove("mystyle");
+    document.getElementById("time-smile").style.display="block";
+    document.getElementById("hide-content").style.opacity="0";
+    document.getElementById("hide-content").style.display="none";
+
+  }  
+  
+   myFunction= ()=> {
+    let d = new Date();
+    let months = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+    let n = months[d.getMonth()];
+    this.setState({
+      date: d.getDate(),
+      month: n
+    })
+  }
+
+  componentDidMount() {
+    this.myFunction();
+  }
+  
   render(){
-    const {content} = this.state;
+    const {content,date,month} = this.state;
   return (
     <div className="App">
+      <div className="list-main">
+        <table>
+      {content.map((string,index)=> index>0?<List key={string.key} identity={string.key} triggerParentUpdate={i => this.delContent(i)} content={string.des} />:null)}
+      </table>
+      </div>
+      <div className="add-div overlay hidden" id='over'>
+        <div id="time-smile">
+
+          <div id="time">
+           <h2 id="date">{date}</h2>
+           <h2 id="month">{month}</h2>
+          </div>
+
+            <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
+            width="98.000000pt" height="134.000000pt" viewBox="0 0 98.000000 134.000000"
+            preserveAspectRatio="xMidYMid meet">
+
+            <g transform="translate(0.000000,134.000000) scale(0.100000,-0.100000)"
+            fill="#000000" stroke="none">
+            <path d="M341 999 c-48 -48 -39 -128 19 -159 45 -24 109 47 110 123 0 67 -77
+            89 -129 36z"/>
+            <path d="M733 808 c-34 -39 -53 -72 -53 -93 0 -31 41 -65 78 -65 64 0 119 122
+            74 163 -27 24 -75 21 -99 -5z"/>
+            <path d="M112 715 c-42 -53 -37 -145 13 -231 31 -53 128 -143 186 -171 110
+            -54 278 -33 362 45 37 35 38 97 2 136 -24 26 -26 26 -142 24 -109 -3 -121 -1
+            -155 20 -21 13 -61 56 -89 95 -29 40 -62 80 -73 89 -32 26 -81 23 -104 -7z"/>
+            </g>
+           </svg>
+        </div>
+      <div id="hide-content">
+        <h2>NEW TASK</h2>
+          <div className="smile">
+
+              <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
+              width="189.000000pt" height="203.000000pt" viewBox="0 0 189.000000 203.000000"
+              preserveAspectRatio="xMidYMid meet">
+
+              <g transform="translate(0.000000,203.000000) scale(0.100000,-0.100000)"
+              fill="#000000" stroke="none">
+              <path d="M1254 1460 c-27 -11 -68 -64 -83 -109 -17 -53 -13 -150 9 -198 46
+              -102 165 -111 216 -16 37 68 48 125 35 194 -12 69 -36 103 -88 125 -36 16 -58
+              17 -89 4z"/>
+              <path d="M502 1435 c-67 -56 -85 -194 -38 -289 31 -63 75 -97 132 -104 39 -4
+              47 -1 78 30 43 43 60 102 54 190 -6 75 -39 143 -87 177 -41 30 -101 28 -139
+              -4z"/>
+              <path d="M800 660 c-25 -25 -26 -72 -1 -126 22 -48 59 -80 112 -95 49 -15 111
+              6 158 55 53 55 54 146 1 146 -10 0 -44 -14 -75 -30 l-58 -30 -15 22 c-28 40
+              -70 78 -86 78 -9 0 -25 -9 -36 -20z"/>
+              </g>
+              </svg>
+
+            </div><br/>
       <input id="todo-input" onChange={e => this.updateText(e)}></input>
-      <button className="todo-submit" onClick={this.addContent}>+</button>
-      <button className="todo-submit" onClick={this.delContent}>-</button>
-      {content.map(string=><List content={string} />)}
+      <br/>
+      <button id="todo-submit" onClick={this.addContent}>SAVE</button>
+      </div>
+      </div>
+      <button id="todo-task" onClick={this.expand}>ADD NEW TASK</button>
     </div>
-    
   );
 }
 }
--- src/List.js
+++ src/List.js
@@ -3,13 +3,49 @@ import './App.css';
 
 class List extends React.Component{
 
+
+
+  toggle_tick = (e) =>{
+    console.log(e.target,e.target.getAttribute('identity'));
+    this.props.triggerParentUpdate(e.target.getAttribute('identity'));   
+  }
+
+  toggle_cross= (e) =>{
+    
+    // console.log( );
+    document.getElementsByClassName(e.target.getAttribute('identity'))[0].style.display = "none";
+    
+    document.getElementById(e.target.getAttribute('identity')).querySelector('p').style.textDecoration = "line-through"; 
+  }
+
+  toggle_text = (e) =>{
+     e.target.style.textDecoration == "line-through"? e.target.style.textDecoration = "none":e.target.style.textDecoration = "line-through";
+  }
+
   render(){
   return (
+      <tr >
     <div className="list">
-      <ul>
-  <li>{this.props.content}</li>
-      </ul>
+    <div className="list-item">
+    <td className="option" >
+      <div  className="op1" key={this.props.key} onClick={e => this.toggle_cross(e)}>
+<svg identity={this.props.identity} className={this.props.identity} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path  identity={this.props.identity} className={this.props.identity}   d="M383.841 171.838c-7.881-8.31-21.02-8.676-29.343-.775L221.987 296.732l-63.204-64.893c-8.005-8.213-21.13-8.393-29.35-.387-8.213 7.998-8.386 21.137-.388 29.35l77.492 79.561a20.687 20.687 0 0014.869 6.275 20.744 20.744 0 0014.288-5.694l147.373-139.762c8.316-7.888 8.668-21.027.774-29.344z"/><path  identity={this.props.identity} className={this.props.identity}   d="M256 0C114.84 0 0 114.84 0 256s114.84 256 256 256 256-114.84 256-256S397.16 0 256 0zm0 470.487c-118.265 0-214.487-96.214-214.487-214.487 0-118.265 96.221-214.487 214.487-214.487 118.272 0 214.487 96.221 214.487 214.487 0 118.272-96.215 214.487-214.487 214.487z"/></svg>
+</div>
+
+<div  className="op2" key={this.props.key} onClick={e => this.toggle_tick(e)}>
+
+<svg identity={this.props.identity} height="512pt" viewBox="0 0 512 512" width="512pt" xmlns="http://www.w3.org/2000/svg"><path identity={this.props.identity} d="m256 512c-141.160156 0-256-114.839844-256-256s114.839844-256 256-256 256 114.839844 256 256-114.839844 256-256 256zm0-475.429688c-120.992188 0-219.429688 98.4375-219.429688 219.429688s98.4375 219.429688 219.429688 219.429688 219.429688-98.4375 219.429688-219.429688-98.4375-219.429688-219.429688-219.429688zm0 0"/><path identity={this.props.identity} d="m347.429688 365.714844c-4.679688 0-9.359376-1.785156-12.929688-5.359375l-182.855469-182.855469c-7.144531-7.144531-7.144531-18.714844 0-25.855469 7.140625-7.140625 18.714844-7.144531 25.855469 0l182.855469 182.855469c7.144531 7.144531 7.144531 18.714844 0 25.855469-3.570313 3.574219-8.246094 5.359375-12.925781 5.359375zm0 0"/><path identity={this.props.identity} d="m164.570312 365.714844c-4.679687 0-9.355468-1.785156-12.925781-5.359375-7.144531-7.140625-7.144531-18.714844 0-25.855469l182.855469-182.855469c7.144531-7.144531 18.714844-7.144531 25.855469 0 7.140625 7.140625 7.144531 18.714844 0 25.855469l-182.855469 182.855469c-3.570312 3.574219-8.25 5.359375-12.929688 5.359375zm0 0"/></svg>
+</div>
+    </td>
+    
+    <td id={this.props.identity}>
+    <p id="text-content"  onClick={e => this.toggle_text(e)}>
+    {this.props.content}
+    </p>
+    </td>
     </div>
+    </div>
+    </tr>
   );
 }
 }
